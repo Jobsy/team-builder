cc08f978297f8595bf487ddc9cb576f48b8a2c64
Oluwajoba <oluwajobabello@gmail.com>
2019-09-18 17:25:26 +0100

    add app.js to the DOm

---
+++ team-builder/src/components/Container.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import uuid from "uuid";
+
+const Container = props => {
+
+    return (
+        
+            <h1>Hello World</h1>
+            
+            );
+          }
+          
+  export default Container;
