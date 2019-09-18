1029d22511559c29e59dec39225411a9dcd2b21f
Oluwajoba <oluwajobabello@gmail.com>
2019-09-18 22:10:58 +0100

    import the useState hook / utilize two-way data binding for state mangement / step-2

--- team-builder/src/components/Container.js
+++ team-builder/src/components/Container.js
@@ -4,27 +4,34 @@ import uuid from "uuid";
 
 
 const initialTeamList = [
-        { id: uuid(), name: "gabe",  email: "gabe@gmail.com" , role: "UX Design"},
-        { id: uuid(), name: "luke",  email: "luke@gmail.com", role: "FrontEnd Engineer"},
-        { id: uuid(), name: "josh",  email: "josh@gmail.com", role: "BackEnd Engineer"},
+        { id: uuid(), name: "gabe", email: "gabe@gmail.com", role: "UX Design" },
+        { id: uuid(), name: "luke", email: "luke@gmail.com", role: "FrontEnd Engineer" },
+        { id: uuid(), name: "josh", email: "josh@gmail.com", role: "BackEnd Engineer" },
 ];
 
+const initialTeamForm = {
+        name: '',
+        email: '',
+        role: '',
+      };
+      
 const Container = props => {
 
-        const [teamList, setTeamList] = useState(initialTeamList)
+        const [teamList, setTeamList] = useState(initialTeamList);
+        const [teamForm, setTeamForm] = useState(initialTeamForm);
 
         return (
-<div>
-                {
-                teamList.map(teamMember => (
-                        <h5 key={teamMember.id}>
-                                {teamMember.name} <br/>
-                                {teamMember.email}<br/>
-                                {teamMember.role}<br/>
-                          </h5>
-                ))
-        }
-</div>
+                <div>
+                        {
+                                teamList.map(teamMember => (
+                                        <h5 key={teamMember.id}>
+                                                {teamMember.name} <br />
+                                                {teamMember.email}<br />
+                                                {teamMember.role}<br />
+                                        </h5>
+                                ))
+                        }
+                </div>
         );
 }
 
