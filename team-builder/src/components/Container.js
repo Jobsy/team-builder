0a7ea07cd1068da73caca8ad894c2c6d70e99506
Oluwajoba <oluwajobabello@gmail.com>
2019-09-18 17:32:25 +0100

    add uuid

--- team-builder/src/components/Container.js
+++ team-builder/src/components/Container.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import uuid from "uuid";
 
+
+
+
 const Container = props => {
 
-    return (
-        
-            <h1>Hello World</h1>
-            
-            );
-          }
-          
-  export default Container;
+        const [teamList, setTeamList] = useState(initialTeamList)
+
+        return (
+
+                <h1>Hello World</h1>
+
+        );
+}
+
+export default Container;
