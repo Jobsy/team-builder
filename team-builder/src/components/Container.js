91696d6d92a63e6e6212b5d3f6d5459a57db6fff
Oluwajoba <oluwajobabello@gmail.com>
2019-09-19 07:15:10 +0100

    create form / add labels and input field to form / step-2

--- team-builder/src/components/Container.js
+++ team-builder/src/components/Container.js
@@ -4,35 +4,79 @@ import uuid from "uuid";
 
 
 const initialTeamList = [
-        { id: uuid(), name: "gabe", email: "gabe@gmail.com", role: "UX Design" },
-        { id: uuid(), name: "luke", email: "luke@gmail.com", role: "FrontEnd Engineer" },
-        { id: uuid(), name: "josh", email: "josh@gmail.com", role: "BackEnd Engineer" },
+  { id: uuid(), name: "gabe", email: "gabe@gmail.com", role: "UX Design" },
+  { id: uuid(), name: "luke", email: "luke@gmail.com", role: "FrontEnd Engineer" },
+  { id: uuid(), name: "josh", email: "josh@gmail.com", role: "BackEnd Engineer" },
 ];
 
 const initialTeamForm = {
-        name: '',
-        email: '',
-        role: '',
-      };
-      
+  name: '',
+  email: '',
+  role: '',
+};
+
 const Container = props => {
 
-        const [teamList, setTeamList] = useState(initialTeamList);
-        const [teamForm, setTeamForm] = useState(initialTeamForm);
+  const [teamList, setTeamList] = useState(initialTeamList);
+  const [teamForm, setTeamForm] = useState(initialTeamForm);
 
-        return (
-                <div>
-                        {
-                                teamList.map(teamMember => (
-                                        <h5 key={teamMember.id}>
-                                                {teamMember.name} <br />
-                                                {teamMember.email}<br />
-                                                {teamMember.role}<br />
-                                        </h5>
-                                ))
-                        }
-                </div>
-        );
+  return (
+    <div>
+      {
+        teamList.map(teamMember => (
+          <h5 key={teamMember.id}>
+            {teamMember.name} <br />
+            {teamMember.email}<br />
+            {teamMember.role}<br />
+          </h5>
+        ))
+      }
+    </div>
+  );
 }
+
+
+
+function Form(props) {
+  const { onNameChange, onEmailChange, onRoleChange, onFormSubmit } = props;
+  const { name, email, role } = props.teamForm;
+};
+
+return (
+  <form>
+    <label htmlFor="nameInput">Name</label>
+    <input
+      maxLength={50}
+      value={name}
+      onchange={onNameChange}
+      id="nameInput"
+      type="text"
+    />
+
+    <label htmlFor="nameInput">Email</label>
+    <input
+      maxLength={50}
+      value={email}
+      onchange={onEmailChange}
+      id="emailInput"
+      type="email"
+    />
+
+    <label htmlFor="nameInput">Role</label>
+    <input
+      maxLength={50}
+      value={role}
+      onchange={onRoleChange}
+      id="roleInput"
+      type="text"
+    />
+
+    
+
+  </form>
+)
+
+
+
 
 export default Container;
